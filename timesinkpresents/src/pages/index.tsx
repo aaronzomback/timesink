e923fad22a140afb3b2923224df81f743418c0f0
Aaron Zomback <aaronzomback@gmail.com>
2023-04-08 19:27:39 +0200

    update: basic mobile landing

--- timesinkpresents/src/pages/index.tsx
+++ timesinkpresents/src/pages/index.tsx
@@ -17,69 +17,63 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className="flex min-h-screen flex-col items-center justify-start bg-dark-theater-primary">
-        <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
-          <h1
-            className={`${LOGO.variable} font-logo text-5xl text-mystery-white sm:text-[5rem]`}
-          >
-            TimeSink Presents
-          </h1>
+        <div className="container flex flex-col items-center justify-center gap-32 px-4 py-16">
+          <div className="flex items-center justify-center">
+            <h1
+              className={`${LOGO.variable} text-center font-logo text-5xl text-mystery-white sm:text-[5rem]`}
+            >
+              TimeSink Presents
+            </h1>
+          </div>
           <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
             <Link
-              className="flex max-w-xs flex-col gap-4 rounded-xl bg-gray-display-heavy p-4 text-mystery-white hover:bg-white/20"
+              className="flex max-w-xs flex-col items-center justify-center gap-4 rounded border-2 border-mystery-white bg-mystery-white p-4 text-dark-theater-primary"
               href="https://create.t3.gg/en/usage/first-steps"
               target="_blank"
             >
-              <h3
-                className={`${OGSANS.variable} font-OGSans text-2xl font-bold`}
-              >
+              <h3 className={`${OGSANS.variable} font-OGSans text-md`}>
                 Cinema →
               </h3>
-              <div className={`${OGSANS.variable} font-OGSans text-md`}>
+              {/* <div className={`${OGSANS.variable} font-OGSans text-md`}>
                 Discover new films made by some of the freshest talent today.
-              </div>
+              </div> */}
             </Link>
             <Link
-              className="flex max-w-xs flex-col gap-4 rounded-xl bg-gray-display-heavy p-4 text-mystery-white hover:bg-white/20"
+              className="flex max-w-xs flex-col items-center justify-center gap-4 rounded border-2 border-mystery-white bg-mystery-white p-4 text-dark-theater-primary drop-shadow-2xl"
               href="https://create.t3.gg/en/introduction"
               target="_blank"
             >
-              <h3
-                className={`${OGSANS.variable} font-OGSans text-2xl font-bold`}
-              >
+              <h3 className={`${OGSANS.variable} font-OGSans text-md`}>
                 Press →
               </h3>
-              <div className={`${OGSANS.variable} font-OGSans text-md`}>
+              {/* <div className={`${OGSANS.variable} font-OGSans text-md`}>
                 Read articles written by our featured and guest writers
-              </div>
+              </div> */}
             </Link>
             <Link
-              className="flex max-w-xs flex-col gap-4 rounded-xl bg-gray-display-heavy p-4 text-mystery-white hover:bg-white/20"
+              className="flex max-w-xs flex-col items-center justify-center gap-4 rounded border-2 border-mystery-white bg-mystery-white p-4 text-dark-theater-primary"
               href="https://create.t3.gg/en/introduction"
               target="_blank"
             >
-              <h3
-                className={`${OGSANS.variable} font-OGSans text-2xl font-bold`}
-              >
+              <h3 className={`${OGSANS.variable} font-OGSans text-md`}>
                 Submit film →
               </h3>
-              <div className={`${OGSANS.variable} font-OGSans text-md`}>
+              {/* <div className={`${OGSANS.variable} font-OGSans text-md`}>
                 Attract audiences by giving your film a chance to be featured in
                 the next release!
-              </div>
+              </div> */}
             </Link>
             <Link
-              className="flex max-w-xs flex-col gap-4 rounded-xl bg-gray-display-heavy p-4 text-mystery-white hover:bg-white/20"
+              className="flex max-w-xs flex-col items-center justify-center gap-4 rounded border-2 border-mystery-white bg-mystery-white p-4 text-dark-theater-primary"
               href="https://create.t3.gg/en/introduction"
               target="_blank"
             >
-              <h3
-                className={`${OGSANS.variable} font-OGSans text-2xl font-bold`}
-              >
-                What is this place? →
+              <h3 className={`${OGSANS.variable} font-OGSans text-md`}>
+                Info →
               </h3>
-              <div className={`${OGSANS.variable} font-OGSans text-md`}>
+              {/* <div className={`${OGSANS.variable} font-OGSans text-md`}>
                 Great question. Let us start by telling you what it is not...
-              </div>
+              </div> */}
             </Link>
           </div>
           <div
@@ -90,6 +84,12 @@ const Home: NextPage = () => {
             </p> */}
             <AuthShowcase />
           </div>
+        </div>
+        <div
+          className={`${OGSANS.variable} flex flex-col items-center justify-center font-OGSans text-sm text-neon-blue-heavy`}
+        >
+          <span>Already a member?</span>
+          <span className="underline">Sign in</span>
         </div>
       </main>
     </>
@@ -107,21 +107,17 @@ const AuthShowcase: React.FC = () => {
   );
 
   return (
-    <div className="flex flex-col items-center justify-center gap-4">
+    <div className="flex flex-col items-center justify-end">
       <p className="text-center text-2xl text-white">
         {sessionData && <span>Logged in as {sessionData.user?.name}</span>}
         {secretMessage && <span> - {secretMessage}</span>}
       </p>
       <button
-        className="rounded border-2 border-neon-blue-heavy px-10 py-3 font-semibold text-neon-blue-heavy no-underline transition hover:bg-neon-blue-heavy hover:text-dark-theater-primary"
+        className="rounded-full border-2 border-neon-blue-heavy px-10 py-3 font-semibold text-neon-blue-heavy no-underline transition hover:bg-neon-blue-heavy hover:text-dark-theater-primary"
         onClick={sessionData ? () => void signOut() : () => void signIn()}
       >
-        {sessionData ? "Sign out" : "Request membership"}
+        {sessionData ? "Sign out" : "Join the waiting list"}
       </button>
-      <div className="flex flex-col items-center justify-center text-sm text-neon-blue-heavy">
-        <span>Already a member?</span>
-        <span className="underline">Sign in</span>
-      </div>
     </div>
   );
 };
